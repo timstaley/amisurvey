6a0fefb39c6ee5de62d4d53995d42751e7a6ea9e
Tim Staley <tim.staley@astro.ox.ac.uk>
2015-01-20 16:11:13 +0000

    Bundle into a package, complete with Binstar deps.
    Also, tox test the install for good measure.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+required = ['simplejson',
+            'colorlog',
+            'drive-ami',
+            'chimenea'
+            ]
+
+setup(
+    name="amisurvey",
+    version="0.1.0",
+    packages=['amisurvey'],
+    scripts=['bin/amisurvey_reduce.py'],
+    description="An end-to-end calibration and imaging pipeline for "
+                "data from the AMI-LA radio observatory.",
+    author="Tim Staley",
+    author_email="timstaley337@gmail.com",
+    url="https://github.com/timstaley/amisurvey",
+    install_requires=required,
+)
