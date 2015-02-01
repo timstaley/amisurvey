715d0485e80dcc5b2953b65c21cded9189a9c544
Tim Staley <tim.staley@astro.ox.ac.uk>
2015-02-01 17:56:06 +0000

    Add primary beam curve calcs (match upstream chimenea changes).

--- amisurvey/amiconfig.py
+++ amisurvey/amiconfig.py
@@ -1,18 +1,21 @@
 """Settings and routines specific to AMI-LA"""
 from __future__ import absolute_import
 from driveami import keys as meta_keys
-
 from chimenea.config import SourcefinderConfig, CleanConfig, ChimConfig
+import numpy as np
 
 import logging
 logger = logging.getLogger(__name__)
 
-image_pixel_size = 512
+image_pixel_dim = 512
+
+pixel_size_arcsec = 5.0
+
 
 ami_clean_args= {
     "spw": '0:0~5',
-    "imsize": [image_pixel_size, image_pixel_size],
-    "cell": ['5.0arcsec'],
+    "imsize": [image_pixel_dim, image_pixel_dim],
+    "cell": [str(pixel_size_arcsec)+'arcsec'],
     "pbcor": False,
     # "weighting": 'natural',
     "weighting": 'briggs',
@@ -31,14 +34,36 @@ sf_conf = SourcefinderConfig(detection_thresh=5., analysis_thresh=3.,
                              radius=None)
 
 
+default_ami_central_freq = 15.37e9
+
+def ami_sigma_arcmin(freq_hz):
+    return 24.905/(freq_hz*1e-9) + 0.79
+
+arcmin_per_pix = pixel_size_arcsec/60
+pb_sigma_arcmin = ami_sigma_arcmin(default_ami_central_freq)
+sigma_pix = pb_sigma_arcmin/arcmin_per_pix
+
+def ami_la_pbcor_curve(radius_pix):
+    return np.exp( -(radius_pix/sigma_pix)**2 / 2.)
+
+cutoff_radius = 3*sigma_pix
+
+####################################################
+
 ami_chimconfig = ChimConfig(clean_conf, sf_conf,
                             max_recleans=3,
                             reclean_rms_convergence=0.05,
                             mask_source_sigma = 5.5,
                             mask_ap_radius_degrees = 60./3600,
+                            pb_correction_curve=ami_la_pbcor_curve,
+                            pb_cutoff_pix=cutoff_radius
                             )
 
+
+
+
 default_rain_min, default_rain_max = 0.8, 1.2
+
 
 def reject_bad_obs(obs_list,
                    rain_min=default_rain_min,
@@ -47,10 +72,8 @@ def reject_bad_obs(obs_list,
     Run quality control on a list of ObsInfo.
 
     Currently just filters on rain gauge values.
-
     Returns 2 lists: [passed],[failed]
     """
-
     good_files = []
     rain_rejected = []
     for obs in obs_list:
