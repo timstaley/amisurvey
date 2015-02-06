96e6c6cb4625fa06d22f1e21f240c9b09eb36d9b
Tim Staley <tim.staley@astro.ox.ac.uk>
2015-02-06 12:33:47 +0000

    Bump chimenea version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ required = ['simplejson',
 
 setup(
     name="amisurvey",
-    version="0.3.2",
+    version="0.3.3",
     packages=['amisurvey'],
     scripts=['bin/amisurvey_reduce.py'],
     description="An end-to-end calibration and imaging pipeline for "
